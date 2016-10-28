7cfee6d78d12c18a4f460ecd19a268b419834744
Harry Wood <mail@harrywood.co.uk>
2016-10-28 16:59:08 +0000

    fix deprecated php7 bits
    I take it somebody upgraded osm dev server to php7.
    Fix a couple of methods which seem to be discontinued

--- schools/index.php
+++ schools/index.php
@@ -76,10 +76,10 @@ while ($data = $results->fetchArray()) {
    $lat          = $data[6];
    $lon          = $data[7];
 
-   $timestamp = eregi_replace("T", " at ", $timestamp);
-   $timestamp = eregi_replace("Z", "", $timestamp);
+   $timestamp = preg_replace("/T/", " at ", $timestamp);
+   $timestamp = preg_replace("/Z/", "", $timestamp);
 
-   $timestamp = eregi_replace("Z", "", $timestamp);
+   $timestamp = preg_replace("/Z/", "", $timestamp);
 
    print "<tr>";
    print "<td>".$timestamp."</td>";
